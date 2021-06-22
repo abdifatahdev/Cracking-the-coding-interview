1004180f37bb77f73c9068f8110689f094564116
Abdifatah Mohamed <mofaatah@gmail.com>
2021-06-22 10:56:40 -0500

    Switched to Java solution

---
+++ chapter1/Solution.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("----------- Test Cases: ----------");
+        Solution s = new Solution();
+        s.solution();
+    }
+
+    public static void solution() {
+        System.out.println("Boilerplate Class");
+    }
+}
