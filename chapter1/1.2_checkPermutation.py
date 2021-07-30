360cca390ffa50eeb0d0faa8ebc52c5d0aef5dc6
Abdifatah Mohamed <mofaatah@gmail.com>
2021-07-30 00:21:07 -0500

    Added a solution

--- chapter1/1.2_checkPermutation.py
+++ chapter1/1.2_checkPermutation.py
@@ -1,29 +1,41 @@
-def checkPermutation(s1, s2):
-    len1 = len(s1)
-    len2 = len(s2)
-
-    if len1 != len2:
+# SOLUTION 1 - O(N LOG N) Time || O(1) Space
+def checkPermutation(string1, string2):
+    if len(string1) != len(string2):
         return False
 
     # Sort both strings
-    str1 = sorted(s1)
-    sorted1 = " ".join(str1)
+    str1 = "".join(sorted(string1))
+    str2 = "".join(sorted(string2))
+    
+    for i in range(0, len(string1), 1):
+        # Compare sorted strings
+        if str1[i] != str2[i]:
+            return False
+    return True
 
-    str2 = sorted(s2)
-    sorted2 = " ".join(str2)
+# SOLUTION 1 - O(N LOG N) Time || O(1) Space
 
-    # Initialize two pointer
-    p1, p2 = 0, 0
 
-    while sorted1[p1] != sorted2[p2]:
+def checkPermutation(word1, word2):
+    if len(word1) != len(word2):
         return False
-    p1 += 1
-    p2 += 1
 
+    seen = {}
+    for ch in word1:
+        seen[ch] = seen.get(ch, 0) + 1
+
+    for ch in word2:
+        if ch in seen:
+            seen[ch] -= 1
+            if seen[ch] == -1: # If seen[ch] < 0:
+                return False
+        else:
+            return False
     return True
 
 
-print(checkPermutation("mom", "oom"))  # True
+print(checkPermutation("mom", "omm"))  # True
+print(checkPermutation("mom", "oom"))  # False
 print(checkPermutation("love", "loove"))  # False => != length
 print(checkPermutation("hello", "olleh"))  # True
 print(checkPermutation("gkla", "lgak"))  # True
