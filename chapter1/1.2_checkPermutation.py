af44ac7dd8fd2c2eec36434d0a095072a4578e94
Abdifatah Mohamed <mofaatah@gmail.com>
2021-08-21 23:55:14 -0500

    Added solution

--- chapter1/1.2_checkPermutation.py
+++ chapter1/1.2_checkPermutation.py
@@ -1,21 +1,4 @@
-# SOLUTION 1 - O(N LOG N) Time || O(1) Space
-def checkPermutation(string1, string2):
-    if len(string1) != len(string2):
-        return False
-
-    # Sort both strings
-    str1 = "".join(sorted(string1))
-    str2 = "".join(sorted(string2))
-    
-    for i in range(0, len(string1), 1):
-        # Compare sorted strings
-        if str1[i] != str2[i]:
-            return False
-    return True
-
-# SOLUTION 1 - O(N LOG N) Time || O(1) Space
-
-
+# SOLUTION 1: O(N) Time and Space
 def checkPermutation(word1, word2):
     if len(word1) != len(word2):
         return False
@@ -27,9 +10,27 @@ def checkPermutation(word1, word2):
     for ch in word2:
         if ch in seen:
             seen[ch] -= 1
-            if seen[ch] == -1: # If seen[ch] < 0:
+            if seen[ch] == -1:  # If seen[ch] < 0:
                 return False
         else:
+            return False
+    return True
+
+#########################################################
+
+
+# SOLUTION 2 - O(N LOG N) Time || O(1) Space
+def checkPermutation(string1, string2):
+    if len(string1) != len(string2):
+        return False
+
+    # Sort both strings
+    str1 = "".join(sorted(string1))
+    str2 = "".join(sorted(string2))
+
+    for i in range(0, len(string1), 1):
+        # Compare sorted strings
+        if str1[i] != str2[i]:
             return False
     return True
 
